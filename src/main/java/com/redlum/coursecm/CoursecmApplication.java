d75f532fc6dea6e21d0c35778494fe92de83f4bd
Matheus J. Mulder <matheusjohanmulder@gmail.com>
2019-04-05 22:26:24 -0300

    Initial Commit

---
+++ src/main/java/com/redlum/coursecm/CoursecmApplication.java
@@ -0,0 +1,13 @@
+package com.redlum.coursecm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursecmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursecmApplication.class, args);
+	}
+
+}
