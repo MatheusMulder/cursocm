a8992939d852607d2826993c1f4aeddc545f720f
Matheus J. Mulder <matheusjohanmulder@gmail.com>
2019-04-05 22:51:02 -0300

    New Method - Category Controller

--- src/main/java/com/redlum/coursecm/resources/CategoryResource.java
+++ src/main/java/com/redlum/coursecm/resources/CategoryResource.java
@@ -1,16 +1,27 @@
 package com.redlum.coursecm.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.redlum.coursecm.model.Category;
 
 @RestController
 @RequestMapping(value="/categories")
 public class CategoryResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String list() {
-		return "REST is working!";
+	public List<Category> list() {
+		Category cat = new Category(1, "Web Services");
+		Category cat1 = new Category(2, "Custom Services");
+		
+		List<Category> listCategory = new ArrayList<>();
+		listCategory.add(cat);
+		listCategory.add(cat1);
+		return  listCategory;
 	}
 	
 
