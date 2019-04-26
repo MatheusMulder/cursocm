7cb0283436c236e5e0defa9934ec7511e8d25fc7
Matheus Mulder <matheusjohanmulder@gmail.com>
2019-04-26 10:43:02 -0300

    Insert data category

--- src/main/java/com/redlum/coursecm/CoursecmApplication.java
+++ src/main/java/com/redlum/coursecm/CoursecmApplication.java
@@ -1,13 +1,32 @@
 package com.redlum.coursecm;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CoursecmApplication {
+import com.redlum.coursecm.model.Category;
+import com.redlum.coursecm.repositories.CategoryRepository;
 
+@SpringBootApplication
+public class CoursecmApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoryRepository cr;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CoursecmApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Category cat1 = new Category(null, "Informática");
+		Category cat2 = new Category(null, "Escritório");
+		
+		cr.saveAll(Arrays.asList(cat1, cat2));
+		
 	}
 
 }
